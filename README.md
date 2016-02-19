98f123171abda1f3b3526fb60cade9b377da8230
Dustin Collins <dustinrcollins@gmail.com>
2016-02-18 21:54:27 -0500

    Add formula

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # homebrew-summon
-Homebrew formula for Summon
+
+[Homebrew](http://brew.sh/) formula for [Summon](https://github.com/conjurinc/summon),
+a command-line tool to make working with secrets easier.
---
+++ summon.rb
@@ -0,0 +1,17 @@
+class Summon < Formula
+  desc "A command-line tool to make working with secrets easier"
+  homepage "https://github.com/conjurinc/summon"
+  url "https://github.com/conjurinc/summon/releases/download/v0.3.2/summon_v0.3.2_darwin_amd64.tar.gz"
+  version "0.3.2"
+  sha256 "2d98e687a65223ec81c950d8d3f7877b7161fc88cd66e2cd85418e03cf9baf3d"
+
+  bottle :unneeded
+
+  def install
+    bin.install "summon"
+  end
+
+  test do
+    system "#{bin}/summon", "-v"
+  end
+end
