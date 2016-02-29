fd65a830896cde740c4aef5c0e6153fcf038c8ac
Dustin Collins <dustinmm80@users.noreply.github.com>
2016-02-29 12:56:09 -0500

    Set correct version for v0.3.3

--- summon.rb
+++ summon.rb
@@ -5,7 +5,7 @@ class Summon < Formula
   desc "Tool to make working with secrets easier"
   homepage "https://github.com/conjurinc/summon"
   url "https://github.com/conjurinc/summon/releases/download/v#{VERSION}/summon_v#{VERSION}_darwin_amd64.tar.gz"
-  version "0.3.2"
+  version VERSION
   sha256 SHA
 
   bottle :unneeded
