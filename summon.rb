43d69b495ed0c8b8568b04b0060c94f0591156e7
Dustin Collins <dustinmm80@users.noreply.github.com>
2017-04-13 09:27:36 -0500

    summon v0.6.4

--- summon.rb
+++ summon.rb
@@ -1,5 +1,5 @@
-VERSION = "0.6.3"
-SHA = "9cc6b118f85195ec13abcc49399411fed1959f30fc43333ca242dccdcbe1781f"
+VERSION = "0.6.4"
+SHA = "28767f28ac6f4e33e5e2bad9f40d2989b51e04ef09c78c88cff28e3f03781481"
 
 class Summon < Formula
   desc "Tool to make working with secrets easier"
