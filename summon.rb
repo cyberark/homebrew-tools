04c4412233957d3bc34f0a2a67c84bfe06048424
Dustin Collins <dustin.collins@cyberark.com>
2018-08-06 15:45:50 -0500

    bump summon to 0.6.7

--- summon.rb
+++ summon.rb
@@ -1,14 +1,9 @@
-VERSION = "0.6.6"
-SHA = "c95e0c72ba9f4262d5d4c737855c234489ff33d4eb5240e1889e37905cd73698"
-
 class Summon < Formula
-  desc "CLI tool that provides on-demand secrets access for common DevOps tools."
+  desc "CLI that provides on-demand secrets access for common DevOps tools."
   homepage "https://github.com/cyberark/summon"
-  url "https://github.com/cyberark/summon/releases/download/v#{VERSION}/summon-darwin-amd64.tar.gz"
-  version VERSION
-  sha256 SHA
-
-  bottle :unneeded
+  url "https://github.com/cyberark/summon/releases/download/v0.6.7/summon-darwin-amd64.tar.gz"
+  version "0.6.7"
+  sha256 "9aad973343db85433e8b5dfa53d4c0f3851073cd2389f1278603b13ae5486fd5"
 
   def install
     bin.install "summon"
