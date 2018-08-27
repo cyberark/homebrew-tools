5aaf6e12ca042c06d50e66843a38020fe61931e9
Dustin Collins <dustin.collins@cyberark.com>
2018-08-27 16:47:18 -0500

    fix dev-flow formula 0.1.0

--- dev-flow.rb
+++ dev-flow.rb
@@ -1,9 +1,9 @@
 class DevFlow < Formula
   desc "CLI for standardizing and automating common development tasks"
   homepage "https://github.com/cyberark/dev-flow"
-  url "https://github.com/cyberark/dev-flow/releases/download/vSNAPSHOT-ed7d79a124d91a29de2934c3879c37d32a4601b7/summon-conjur-darwin-amd64.tar.gz"
-  version "SNAPSHOT-ed7d79a124d91a29de2934c3879c37d32a4601b7"
-  sha256 "6b3e37af71111e5a39201651f0f0f055afbfc7978baa133c8506c1b4c2c792a8"
+  url "https://github.com/cyberark/dev-flow/releases/download/v0.1.0/summon-conjur-darwin-amd64.tar.gz"
+  version "0.1.0"
+  sha256 "bdadbf5c0c8811971f2323acc1a89efca7c8b7e26d26d0d706163017ec78f751"
 
   def install
     bin.install "dev-flow"
