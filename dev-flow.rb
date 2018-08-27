1867a45b8fcad8181a22cb60ec0e14862d48f1f3
Dustin Collins <dustin.collins@cyberark.com>
2018-08-27 16:57:00 -0500

    fix dev-flow homebrew url, again

--- dev-flow.rb
+++ dev-flow.rb
@@ -1,9 +1,9 @@
 class DevFlow < Formula
   desc "CLI for standardizing and automating common development tasks"
   homepage "https://github.com/cyberark/dev-flow"
-  url "https://github.com/cyberark/dev-flow/releases/download/v0.1.0/summon-conjur-darwin-amd64.tar.gz"
+  url "https://github.com/cyberark/dev-flow/releases/download/v0.1.0/dev-flow-darwin-amd64.tar.gz"
   version "0.1.0"
-  sha256 "bdadbf5c0c8811971f2323acc1a89efca7c8b7e26d26d0d706163017ec78f751"
+  sha256 "e2f5132cadf1a2ae2f20b89ec648c5026807af33a28ecd5457f771381b220b41"
 
   def install
     bin.install "dev-flow"
