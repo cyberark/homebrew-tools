8e36442668653fadd6c07e2f8a69e49cd27c1961
Srdjan Grubor <sgnn7@sgnn7.org>
2018-12-07 09:57:47 -0600

    Update summon to v0.6.9 (#1)

--- summon.rb
+++ summon.rb
@@ -1,9 +1,9 @@
 class Summon < Formula
   desc "CLI that provides on-demand secrets access for common DevOps tools."
   homepage "https://github.com/cyberark/summon"
-  url "https://github.com/cyberark/summon/releases/download/v0.6.8/summon-darwin-amd64.tar.gz"
-  version "0.6.8"
-  sha256 "3b3f9124970c711994625fd0f855dfaf25f403f4ed9a82ed570249ec439c1d3b"
+  url "https://github.com/cyberark/summon/releases/download/v0.6.9/summon-darwin-amd64.tar.gz"
+  version "0.6.9"
+  sha256 "5e480dc875f7160ac00b8be370ddc444096b6655919e89a166dde23a9101105b"
 
   def install
     bin.install "summon"
