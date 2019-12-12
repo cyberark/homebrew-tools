c27e199ae55f4571a2ec6ad18b89a0e286757e80
Geri Jennings <geri.jennings@cyberark.com>
2019-12-12 12:46:57 -0500

    Bump Secretless binaries to v1.4.1

--- secretless-broker.rb
+++ secretless-broker.rb
@@ -2,16 +2,16 @@
 class SecretlessBroker < Formula
   desc "Secures your apps by making them Secretless"
   homepage "https://secretless.io"
-  version "1.3.0"
+  version "1.4.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cyberark/secretless-broker/releases/download/v1.3.0/secretless-broker_1.3.0_darwin_amd64.tar.gz"
-    sha256 "1d0cc09a16197975524b9d3124c285fe11e4e522850d75fc8cddd48a68c3d617"
+    url "https://github.com/cyberark/secretless-broker/releases/download/v1.4.1/secretless-broker_1.4.1_darwin_amd64.tar.gz"
+    sha256 "0b274c6f4b9189604f255365b1f348440a81ba7ead6f636effa821a9f399293d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cyberark/secretless-broker/releases/download/v1.3.0/secretless-broker_1.3.0_linux_amd64.tar.gz"
-      sha256 "bddb2936e463c997ce9fb2a2e1ae365fe2fd9f56e6728251a720270d272ffdfb"
+      url "https://github.com/cyberark/secretless-broker/releases/download/v1.4.1/secretless-broker_1.4.1_linux_amd64.tar.gz"
+      sha256 "9ec6f07d18e582cb1f8f2550fda9e20da93cd262859ef8697380cf663c58ef00"
     end
   end
 
