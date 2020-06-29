ab3cc39f6e254caeaad86ce12e6a9d783250df6b
Bradley Boutcher <btboutcher@icloud.com>
2020-06-29 10:24:58 -0400

    Bump summon-conjur to 0.5.3

--- summon-conjur.rb
+++ summon-conjur.rb
@@ -1,9 +1,9 @@
 class SummonConjur < Formula
   desc "Conjur provider for Summon"
   homepage "https://github.com/cyberark/summon-conjur"
-  url "https://github.com/cyberark/summon-conjur/releases/download/v0.5.2/summon-conjur-darwin-amd64.tar.gz"
-  version "0.5.2"
-  sha256 "797cff087b048b3f8400a1b03fd342391c54d39cd7cf734602983cc8cc80b972"
+  url "https://github.com/cyberark/summon-conjur/releases/download/v0.5.3/summon-conjur-darwin-amd64.tar.gz"
+  version "0.5.3"
+  sha256 "c878a4949e0202363382df8646be537684576f8245e4b04bcbc79fce640dc8eb"
 
   def install
     target = lib/"summon"
