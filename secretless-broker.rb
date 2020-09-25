eab26e25d1e571266dc47cac4d2797ba1a63434d
Geri Jennings <geri.jennings@cyberark.com>
2020-09-25 15:42:30 -0400

    Update Secretless to 1.7.0

--- secretless-broker.rb
+++ secretless-broker.rb
@@ -2,16 +2,16 @@
 class SecretlessBroker < Formula
   desc "Secures your apps by making them Secretless"
   homepage "https://secretless.io"
-  version "1.6.0"
+  version "1.7.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cyberark/secretless-broker/releases/download/v1.6.0/secretless-broker_1.6.0_darwin_amd64.tar.gz"
-    sha256 "6757e4fcb249bb680a1a2f45f13404fcf7ed710aad1858bf0efc622fc346407f"
+    url "https://github.com/cyberark/secretless-broker/releases/download/v1.7.0/secretless-broker_1.7.0_darwin_amd64.tar.gz"
+    sha256 "ef78716c0e5734b31d13b5d96f0dd7f5983637f10ad728a411ef32ad3d9d8e3f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cyberark/secretless-broker/releases/download/v1.6.0/secretless-broker_1.6.0_linux_amd64.tar.gz"
-      sha256 "71f38755054454c23ae37c3c800388c5d3751f9d706a481f7b0dab14a7222999"
+      url "https://github.com/cyberark/secretless-broker/releases/download/v1.7.0/secretless-broker_1.7.0_linux_amd64.tar.gz"
+      sha256 "84cb5fa944e65790aa1cccc080349bf0f052c851c9ff5fbe2168291e3e190186"
     end
   end
 
